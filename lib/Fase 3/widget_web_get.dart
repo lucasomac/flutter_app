9f422435f61a99ac0c9276ffb900bf762511cfff
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-12-21 12:32:07 -0300

    Fase 3 - Exibindo dados da web em um ListView

--- lib/Fase 3/widget_web_get.dart
+++ lib/Fase 3/widget_web_get.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 var url = "https://unsplash.com/napi/photos/Q14J2k8VE3U/related";
@@ -8,8 +11,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List data;
+  Future<String> getJSONData() async {
+    final String url = "https://unsplash.com/napi/photos/Q14J2k8VE3U/related";
+    var response = await http.get(url);
+    setState(() {
+      data = json.decode(response.body)['results'];
+    });
+    return "Dados obtidos ocm sucesso";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Http - Json"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: _listView(),
+    );
+  }
+
+  _listView() {
+    return ListView.builder(
+      itemCount: data == null ? 0 : data.length,
+      itemBuilder: (context, index) {
+        return _exibirImagem(data[index]);
+      },
+    );
+  }
+
+  _exibirImagem(dynamic item) => Container(
+        decoration: BoxDecoration(color: Colors.white),
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              imageUrl: item['urls']['small'],
+              placeholder: (context, url) => CircularProgressIndicator(
+                backgroundColor: Colors.blueAccent,
+              ),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fadeOutDuration: Duration(seconds: 2),
+              fadeInDuration: Duration(seconds: 3),
+            ),
+            _criaLinhaTexto(item),
+          ],
+        ),
+      );
+
+  _criaLinhaTexto(dynamic item) {
+    return ListTile(
+      title: Text(item['description'] == null
+          ? "Sem descrição fornecida"
+          : item['description']),
+      subtitle: Text("Likes : " + item['likes'].toString()),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getJSONData();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:http/http.dart' as http;
+import 'Fase 3/widget_web_get.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,37 +14,5 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(primaryColor: Colors.blue),
       home: Home(),
     );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List data;
-  Future<String> getJSONData() async {
-    final String url = "https://unsplash.com/napi/photos/Q14J2k8VE3U/related";
-    var response = await http.get(url);
-    data = json.decode(response.body);
-    print(data);
-    return "Dados obtidos ocm sucesso";
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text("Http - Json"),
-      centerTitle: true,
-      backgroundColor: Colors.blue,
-    ));
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
   }
 }
